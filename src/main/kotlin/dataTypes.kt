e76b594f076eb030778d731fbaf28abed98c6268
jcanaviri <jcanaviri20@gmail.com>
2021-06-22 12:08:22 -0400

    Number and Integer types

---
+++ src/main/kotlin/dataTypes.kt
@@ -0,0 +1,7 @@
+fun main() {
+    // Number and Integer types
+    val myByte: Byte = 8 // 8-bit signed integer
+    val myShort: Short = 16 // 16-bit signed integer
+    val myInt: Int = 32 // 32-bit signed integer
+    var myLong: Long = 64 // 64-bit signed integer
+}
