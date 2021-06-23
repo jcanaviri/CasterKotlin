daeaee8ca02a0e05a37c80fd3550f70f1fbd87a6
jcanaviri <jcanaviri20@gmail.com>
2021-06-22 20:32:03 -0400

    if statement

---
+++ src/main/kotlin/ifStatement.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val number = -12
+    if (number > 0) {
+        println("Is positive")
+    } else if (number < 0) {
+        println("Is negative")
+    } else {
+        println("Is zero")
+    }
+}
