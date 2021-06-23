b64673a927e7e7465da21308f9fd7bf29c050cdd
jcanaviri <jcanaviri20@gmail.com>
2021-06-23 17:08:22 -0400

    The elvis operator
    References to: #3

--- src/main/kotlin/nullableValues.kt
+++ src/main/kotlin/nullableValues.kt
@@ -5,4 +5,10 @@ fun main() {
     // if name is null then null
     // if name is not null then the length
     println(name?.length)
+
+    // Elvis Operator ?:
+    var lastName: String? = "Canaviri"
+    var length: Int = if (lastName != null) lastName.length else 0
+    var length2: Int = lastName?.length ?: 0
+    println(length2)
 }
